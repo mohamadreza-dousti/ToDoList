137a7ecb0c260a9af0027be5894cd9b1e4b98fc5
mohamadreza <mohamaddoosti@gmail.com>
2026-02-08 18:12:14 +0330

    add state variable

--- manageTask/task.py
+++ manageTask/task.py
@@ -1,12 +1,22 @@
+import customtkinter as ctk
+import json
+
 class Task():
     def __init__(self, name, description, priority):
         self.name = name
         self.description = description
         self.priority = priority
+        self.check_var = ctk.StringVar(value='No')
 
     def CreateTask(self):
         file_name = f'{self.name}.txt'
         with open(f'manageTask/tasks_folder/{file_name}', "a+") as file:
             file.write(f'priority : {self.priority}\n')
             file.write(self.description)
-
+        file_name_state = f'manageTask/status_setting/{self.name}.json'
+        data = {
+            "status" : self.check_var.get()
+        }
+        with open(file_name_state, "w") as f:
+            json.dump(data, f)
+            
