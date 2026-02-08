5b5d6476d067d7b800d85f52c8a31e46f0e3e697
mohamadreza <mohamaddoosti@gmail.com>
2026-02-08 23:04:24 +0330

    edit priority widjet:change to option menu, add priority labels

--- UI/ctk.py
+++ UI/ctk.py
@@ -6,7 +6,7 @@ class Ui(ctk.CTk):
         super().__init__()
 
         self.title("CREATED BY DOUSTI")
-        self.geometry('400x400')
+        self.geometry('500x400')
 
         self.add = ctk.CTkButton(self, text='add task', corner_radius=10, width=100, height=40,
         command=self.AddTask)
@@ -16,7 +16,7 @@ class Ui(ctk.CTk):
         command=self.ShowTasks)
         self.show.pack(pady=10)
 
-        self.display = ctk.CTkScrollableFrame(self, width=350, height=250,
+        self.display = ctk.CTkScrollableFrame(self, width=450, height=250,
                                             corner_radius=10)
         self.display.pack(pady=5)
     
@@ -51,8 +51,10 @@ class Ui(ctk.CTk):
                                 border_width=2)
         text_box.pack(pady=10)
 
-        priority = ctk.CTkEntry(self.display, width=350, height=40,corner_radius=10,
-                            placeholder_text="priority")
+        values = ['very high', 'high', 'mid', 'low']
+        priority_var = ctk.StringVar(value='priority')
+        priority = ctk.CTkOptionMenu(self.display, width=350, height=40,corner_radius=10,
+                                     variable=priority_var, values=values)
         priority.pack(pady=10)
 
         save_btn = ctk.CTkButton(self.display, text='Save', command=lambda:newTask.AddTask(title_entry, text_box, priority, save_btn))
@@ -74,35 +76,39 @@ class Ui(ctk.CTk):
         self.display.grid_columnconfigure(2, weight=1)
         self.display.grid_columnconfigure(3, weight=1)
         self.display.grid_columnconfigure(4, weight=1)
+        self.display.grid_columnconfigure(5, weight=1)
 
         ctr = 0
         remove_vars = {}
         show_vars = {}
         status_vars = {}
         check_var = {}
+        priority_vars = {}
         for task in tasks:
             task_label = ctk.CTkLabel(self.display, text=task, width=80)
-            task_label.grid(row=ctr, column=0)
+            task_label.grid(row=ctr, column=0, columnspan=2)
 
             remove_vars[f'remove_btn{ctr}'] = ctk.CTkButton(self.display, text='remove', width=80,
             command=lambda i=ctr : dirlist.Remove(self.display, remove_vars[f'remove_btn{i}'], show_vars[f'show_task{i}']))
-            remove_vars[f'remove_btn{ctr}'].grid(row=ctr, column=1)
+            remove_vars[f'remove_btn{ctr}'].grid(row=ctr, column=2)
 
             show_vars[f'show_task{ctr}'] = ctk.CTkButton(self.display, text='show', width=80,
             command=lambda i=ctr : dirlist.showTask(self.display, show_vars[f'show_task{i}']))
-            show_vars[f'show_task{ctr}'].grid(row=ctr, column=2)
+            show_vars[f'show_task{ctr}'].grid(row=ctr, column=3)
 
             check_var[f'var{ctr}'] = ctk.StringVar(value='No')
             status_vars[f'status{ctr}'] = ctk.CTkCheckBox(self.display, text='DONE', variable=check_var[f'var{ctr}'], offvalue='No', onvalue='Yes',
                                                           command=lambda i=ctr : dirlist.set(status_vars[f'status{i}'], self.display))
-            status_vars[f'status{ctr}'].grid(row=ctr, column=3, padx=2)
+            status_vars[f'status{ctr}'].grid(row=ctr, column=4, padx=2)
             dirlist.status(status_vars[f'status{ctr}'], self.display, check_var[f'var{ctr}'])
+
+            dirlist.save_priority(task_label, self.display, ctr, priority_vars)
 
             ctr += 1
 
         back_btn = ctk.CTkButton(self.display, text='back', command=lambda:self.Back(back_btn))
-        back_btn.grid(row=ctr,column=0, columnspan=2, pady=15)
+        back_btn.grid(row=ctr,column=0, columnspan=3, pady=15)
 
         refresh_btn = ctk.CTkButton(self.display, text='refresh🔄️', command=self.Refresh)
-        refresh_btn.grid(row=ctr, column=3, columnspan=2)
+        refresh_btn.grid(row=ctr, column=3, columnspan=3)
 
